d6f07a910921c4b3deb6ae8f4f9f5f7d8b33d719
sanath <sanaabdulmanaf88@gmail.com>
2024-03-21 07:32:25 +0400

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Courses from "./pages/Courses";
+import Tutors from "./pages/Tutors";
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/courses" element={<Courses />}></Route>
+        <Route path="/tutors" element={<Tutors />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
