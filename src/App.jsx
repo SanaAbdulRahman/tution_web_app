2ffc5bf7e49b8fa6b1924c9507815611cc87a654
sanath <sanaabdulmanaf88@gmail.com>
2024-03-26 17:31:37 +0400

    create landing page and login page

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,26 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Courses from "./pages/Courses";
-import Tutors from "./pages/Tutors";
+import Tutor from "./pages/Tutor";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/courses" element={<Courses />}></Route>
-        <Route path="/tutors" element={<Tutors />}></Route>
+        <Route path="/tutors" element={<Tutor />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
