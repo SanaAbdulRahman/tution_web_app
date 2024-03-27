a67fc84816e9e3c4a0f0b941d0646896de6e7ba0
sanath <sanaabdulmanaf88@gmail.com>
2024-03-27 15:22:58 +0400

    create login and sign up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
+import SignIn from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Courses from "./pages/Courses";
 import Tutor from "./pages/Tutor";
-
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/courses" element={<Courses />}></Route>
-        <Route path="/tutors" element={<Tutor />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
+        <Route
+          path="/*"
+          element={
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/tutors" element={<Tutor />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+              </Routes>
+            </Layout>
+          }
+        />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,69 @@
 import React from "react";
 
-export default function SignUp() {
-  return <div>Sign Up</div>;
-}
+import { GiGraduateCap } from "react-icons/gi";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  return (
+    <div className="py-6">
+      <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+        <div
+          className="hidden lg:block lg:w-1/2 bg-cover"
+          style={{
+            backgroundImage: "url('src/assets/images/slider/gf.jpg')",
+          }}
+        ></div>
+        <div className="w-full p-8 lg:w-1/2">
+          <Link to="/">
+            <h1 className="font-bold text-sm sm:text-xl flex text-center">
+              <span className="text-green-400">Tutor</span>
+              <span className="text-green-700">Eye</span>
+              <GiGraduateCap />
+            </h1>
+          </Link>
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Full Name
+            </label>
+            <input
+              className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+              type="text"
+            />
+          </div>
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Email Address
+            </label>
+            <input
+              className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+              type="email"
+            />
+          </div>
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+              type="password"
+            />
+          </div>
+          <div className="mt-8">
+            <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
+              Sign Up
+            </button>
+          </div>
+          <div className="mt-4 flex items-center justify-between">
+            <span className="border-b w-1/5 md:w-1/4"></span>
+            <Link to="/sign-in" className="text-xs text-gray-500 uppercase">
+              Back to Login
+            </Link>
+            <span className="border-b w-1/5 md:w-1/4"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
